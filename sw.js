855a8db4a37fa057cdd421760635f054d8ede8f7
imperialempy-collab <imperial.empy@gmail.com>
2026-02-27 11:16:34 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,3 @@
-// 1. Cargamos el motor de OneSignal
-importScripts("https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.sw.js");
-
-// 2. Cargamos el motor para que la AppWeb se pueda instalar (PWA)
 self.addEventListener('fetch', (event) => {
-  event.respondWith(fetch(event.request));
+  event.respondWith(fetch(event.request).catch(() => new Response('Offline')));
 });
